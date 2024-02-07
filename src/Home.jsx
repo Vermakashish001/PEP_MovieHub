59b0bed369f094d9d2730b08c1e12285395188fc
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-07 23:47:42 +0530

    Commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,9 +4,18 @@ import { useNavigate } from 'react-router-dom'
 export default function Home() {
     const navigate = useNavigate()
   return (
+    
     <div className='homebtns'>
-        <button onClick={()=>navigate("/movies")}>Movies</button>
-        <button onClick={()=>navigate("/tv")}>Tv</button>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpEIytwvMoIoiEKGU9-tqxI6s4p3E88Lcfyw&usqp=CAU"
+        alt="Movies"
+        onClick={() => navigate("/movies")}
+      />
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrQKG7otAOxz4o__M6qGngh8ZgFTkEjl_zxA&usqp=CAU"
+        alt="Tv"
+        onClick={() => navigate("/tv")}
+      />
     </div>
   )
 }
