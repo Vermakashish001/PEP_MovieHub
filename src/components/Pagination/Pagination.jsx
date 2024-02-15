0c6cde840b8c6541c9e4f88dbb5dcf9a7ae48cd9
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-15 15:12:44 +0530

    Commit

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,41 +1,25 @@
 import React from 'react'
+import './Pagination.css'
 
 export default function Pagination({currentPage,totalPages,onPageChange}) {
-    // console.log(totalPages)
-    // const pageNumbers = []
-    // for(let i=0;i<totalPages;i++){
-    //     pageNumbers.push(i)
-    // }
-    console.log(currentPage)
     const maxButtons=5;
     const calculateButtonRange =()=>{
-        const halfMaxButtons = Math.floor(maxButtons/2);
-        let start = currentPage - halfMaxButtons;
-        let end = currentPage+halfMaxButtons;
-        if(start < 1){
-            start = 1;
-            end = start + maxButtons -1 ;
-        }
+        let start = 1;
+        let end = start + maxButtons - 1;
         if(end > totalPages){
             end = totalPages;
-            start = end - maxButtons + 1;
-            if(start <1){
-                start =1;
-            }
         }
         return {start,end}
     }
    const {start,end}= calculateButtonRange();
-   console.log(start,end)
    const pageNumbers = Array.from({length:end-start+1},(_,index)=>start+index);
-//    console.log(pageNumbers)
   return (
     <div className='pagination'>
-        <button onClick={()=>onPageChange(currentPage-1)}>Previous</button>
+        <button className='page-button nav-prev' onClick={()=>onPageChange(currentPage-1)} disabled={currentPage === 1}>Previous</button>
         {pageNumbers.map((page)=>(
-            <button onClick={()=>onPageChange(page) } disabled={currentPage===page}>{page}</button>
+            <button className={`page-button ${currentPage === page ? 'active' : ''}`} onClick={()=>onPageChange(page)}>{page}</button>
         ))}
-        <button onClick={()=>onPageChange(currentPage+1)}>Next</button>
+        <button className='page-button nav-next' onClick={()=>onPageChange(currentPage+1)} disabled={currentPage === totalPages}>Next</button>
     </div>
   )
 }
