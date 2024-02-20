ee59d75b62ac9e8f7daae0af4acfc7838c1fe3e7
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-02-20 22:22:18 +0530

    Commit

--- src/components/Cards/MovieCard.jsx
+++ src/components/Cards/MovieCard.jsx
@@ -1,29 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import { FaHeart, FaRegHeart } from "react-icons/fa"
-import './Card.css'
+import React, { useEffect, useState } from 'react';
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import './Card.css';
 
 export default function MovieCard({ movie }) {
-
   const { id, poster_path, title, release_date, vote_count, vote_average } = movie;
   const [isFavorite, setIsFavorite] = useState(
     JSON.parse(localStorage.getItem('favoriteMovies'))?.some(([_, movieId]) => movieId === id) || false
-  )
+  );
+  const [showDetails, setShowDetails] = useState(false);
+
   const toggleFavorite = () => {
-    setIsFavorite((prev) => !prev)
-  }
+    setIsFavorite((prev) => !prev);
+  };
+
   useEffect(() => {
-    const favMovies = JSON.parse(localStorage.getItem("favoriteMovies")) || []
+    const favMovies = JSON.parse(localStorage.getItem("favoriteMovies")) || [];
     if (isFavorite) {
-      localStorage.setItem('favoriteMovies', JSON.stringify([...favMovies, [title, id]]))
+      localStorage.setItem('favoriteMovies', JSON.stringify([...favMovies, [title, id]]));
     } else {
       localStorage.setItem('favoriteMovies',
-        JSON.stringify(favMovies.filter(([_, movieId]) => movieId !== id)))
+        JSON.stringify(favMovies.filter(([_, movieId]) => movieId !== id)));
     }
-  }, [isFavorite, title, id])
+  }, [isFavorite, title, id]);
 
   return (
     <div>
-      <li className='card'>
+      <li className='card' onMouseEnter={() => setShowDetails(true)} onMouseLeave={() => setShowDetails(false)}>
         <img className='poster' src={`https://image.tmdb.org/t/p/original/${poster_path}`} alt='title' />
         <h1 className='title'>{title}</h1>
         <div className='likebtn'>
@@ -33,7 +35,12 @@ export default function MovieCard({ movie }) {
             <FaHeart className='fav-icon' onClick={toggleFavorite} />
           )}
         </div>
+        <div className={`card-details ${showDetails ? 'visible' : ''}`}>
+          <p><strong>Release Date:</strong> {release_date}</p>
+          <p><strong>Vote Count:</strong> {vote_count}</p>
+          <p><strong>Vote Average:</strong> {vote_average}</p>
+        </div>
       </li>
     </div>
-  )
+  );
 }
--- src/components/Cards/TvCard.jsx
+++ src/components/Cards/TvCard.jsx
@@ -1,27 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import { FaHeart, FaRegHeart } from "react-icons/fa"
-import './Card.css'
-export default function TvCard({ tv }) {
+import React, { useEffect, useState } from 'react';
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import './Card.css';
 
+export default function TvCard({ tv }) {
   const { id, poster_path, name, first_air_date, vote_count, vote_average } = tv;
   const [isFavorite, setIsFavorite] = useState(
     JSON.parse(localStorage.getItem('favoriteSeries'))?.some(([_, movieId]) => movieId === id) || false
-  )
+  );
+  const [showDetails, setShowDetails] = useState(false); // Added this line
+
   const toggleFavorite = () => {
-    setIsFavorite((prev) => !prev)
-  }
+    setIsFavorite((prev) => !prev);
+  };
+
   useEffect(() => {
-    const favSeries = JSON.parse(localStorage.getItem("favoriteSeries")) || []
+    const favSeries = JSON.parse(localStorage.getItem("favoriteSeries")) || [];
     if (isFavorite) {
-      localStorage.setItem('favoriteSeries', JSON.stringify([...favSeries, [name, id]]))
+      localStorage.setItem('favoriteSeries', JSON.stringify([...favSeries, [name, id]]));
     } else {
       localStorage.setItem('favoriteSeries',
-        JSON.stringify(favSeries.filter(([_, tvId]) => tvId !== id)))
+        JSON.stringify(favSeries.filter(([_, tvId]) => tvId !== id)));
     }
-  }, [isFavorite, name, id])
+  }, [isFavorite, name, id]);
+
   return (
     <div>
-      <li className='card'>
+      <li className='card' onMouseEnter={() => setShowDetails(true)} onMouseLeave={() => setShowDetails(false)}>
         <img className='poster' src={`https://image.tmdb.org/t/p/original/${poster_path}`} alt='title' />
         <h1 className='title'>{name}</h1>
         <div className='likebtn'>
@@ -31,7 +35,12 @@ export default function TvCard({ tv }) {
             <FaHeart className='fav-icon' onClick={toggleFavorite} />
           )}
         </div>
+        <div className={`card-details ${showDetails ? 'visible' : ''}`}>
+          <p><strong>First Air Date:</strong> {first_air_date}</p>
+          <p><strong>Vote Count:</strong> {vote_count}</p>
+          <p><strong>Vote Average:</strong> {vote_average}</p>
+        </div>
       </li>
     </div>
-  )
+  );
 }
